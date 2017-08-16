9df8af23474b8c94cca6aa0cc2eea990a1c179af
timbert@adobe.com <9f9d51bc>
2017-08-16 00:10:17 -0700

    Locking to portrait mode

--- Beacon/AppDelegate.swift
+++ Beacon/AppDelegate.swift
@@ -14,7 +14,11 @@ import UserNotifications
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
+    
+    func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
+        return UIInterfaceOrientationMask(rawValue: UIInterfaceOrientationMask.portrait.rawValue)
+    }
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         // iOS 10 support
